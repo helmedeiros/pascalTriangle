998dc27ecd8b2e64c5ab3691164f7440b55c526c
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-06-05 23:39:24 -0300

    Initial version

---
+++ lib/pascals_triangle.rb
@@ -0,0 +1,24 @@
+class PascalsTriangle
+  def initialize
+    @last_row = nil
+  end
+  
+  def next_row
+    @last_row = if @last_row.nil?
+      [1]
+    elsif @last_row == [1]
+      [1, 1]
+    else
+      [1] + @last_row.enum_for(:each_cons, 2).map { |l, r| l+r} + [1]
+    end    
+  end
+  
+  def each(limit = nil)
+    if limit.nil?
+      0.upto(1.0/0.0) { yield next_row}
+    else
+      limit.times { yield next_row }
+    end
+  end
+  
+end
---
+++ test/tc_pascals_triangle.rb
@@ -0,0 +1,50 @@
+require "test/unit"
+
+require "pascals_triangle"
+
+class TestPascalsTriangle < Test::Unit::TestCase
+  def setup
+    @output = <<END_OUTPUT
+                           1                         
+                        1     1                      
+                     1     2     1                   
+                  1     3     3     1                
+               1     4     6     4     1             
+            1     5    10    10     5     1          
+         1     6    15    20    15     6     1       
+      1     7    21    35    35    21     7     1    
+   1     8    28    56    70    56    28     8     1
+1     9    36    84    126   126   84    36     9     1
+END_OUTPUT
+
+    @rows = @output.map { |line| line.scan(/\d+/).map { |n| n.to_i  }  }
+    @rows.each do |row|
+      puts "[#{row}]"
+    end
+  
+    @tri = PascalsTriangle.new
+  end  
+  
+  def test_next_row
+    @rows.each do |row|
+      assert_equal(row, @tri.next_row)
+    end
+  end
+  
+  def test_each
+    @tri.each(1) do |row|
+      assert_equal(@rows.shift, row)
+    end
+    
+     @tri.each(9) do |row|
+       puts row
+       @temp = @rows.shift
+       puts "#{@temp}"  
+        assert_equal(@temp, row)
+      end
+  end
+  
+  def test_find
+    assert_equal([1, 5, 10, 10, 5, 1 ], @tri.find { |row| row.any? { |n| n > 9 }  })
+  end
+end
