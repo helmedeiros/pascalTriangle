f9a4b57b54672c194cad340dd4047127635764f6
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-06-09 00:58:58 -0300

    Finished the pretty printing on pascal triangles, now is easy to create and format them

--- test/tc_pascals_triangle.rb
+++ test/tc_pascals_triangle.rb
@@ -9,16 +9,16 @@ require "pascals_triangle"
 class TestPascalsTriangle < Test::Unit::TestCase
   def setup
     @output = <<END_OUTPUT
-                           1                         
-                        1     1                      
-                     1     2     1                   
-                  1     3     3     1                
-               1     4     6     4     1             
-            1     5    10    10     5     1          
-         1     6    15    20    15     6     1       
-      1     7    21    35    35    21     7     1    
-   1     8    28    56    70    56    28     8     1
-1     9    36    84    126   126   84    36     9     1
+                            1                            
+                         1     1                         
+                      1     2     1                      
+                   1     3     3     1                   
+                1     4     6     4     1                
+             1     5    10    10     5     1             
+          1     6    15    20    15     6     1          
+       1     7    21    35    35    21     7     1       
+    1     8    28    56    70    56    28     8     1    
+ 1     9    36    84    126   126   84    36     9     1
 END_OUTPUT
 
     @rows = @output.each_line.map { |line| line.scan(/\d+/).map { |n| n.to_i  }  }
